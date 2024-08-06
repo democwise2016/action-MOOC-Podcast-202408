176fa1c21ea46a800c5d9cac90e86559e92190a2
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-08-06 21:28:33 +0800

    西元2024年08月06日 (週二) 21時28分33秒 CST

---
+++ config-json.js
@@ -0,0 +1,24 @@
+let CONFIG = {
+  publicURL: `https://democwise2016.github.io/action-MOOC-Podcast-202408/`,
+  publicURLShorten: `https://rb.gy/74b6e7`,
+  thumbnailBorderColor: '44003A',
+  titleAppend: '[MC+202408]',
+  titleSuffix: '[MC+202408]',
+  idAppend: false,
+
+  maxDownloadItemPerFeed: 500000,
+  maxDownloadFeed: 5000000,
+  newArrialMax: 500000,
+  maxDownloadItems: 1000000,
+  maxExcutionMinutes: 15,
+  maxSameTime: 2,
+}
+
+// let test = '1780'
+// CONFIG.publicURL = `https://dangerous-lizard-99.telebit.io/`
+// CONFIG.titleAppend = '[MC+] 20240125-' + test
+// CONFIG.idAppend = '20240125-' + test
+// CONFIG.maxDownloadItemPerFeed = 100
+// CONFIG.debug = true
+
+module.exports = CONFIG
